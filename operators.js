5edfe07549e32d415c2c49fb7d3025b0ea4ab1e9
khloe <slowykhloe@gmail.com>
2023-02-06 09:52:42 -0800

    initial commit

---
+++ operators.js
@@ -0,0 +1,57 @@
+// **unary operators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+
+// **binary operators use two values
+// comparison operators
+console.log(10 > 5);  //greater than
+console.log(10 < 5); //less than
+console.log(10 >= 5); //greater than or equal to
+console.log(10 <= 5); //less than or equal to
+console.log(10 == 5); //are they both equal? false
+console.log(10 == 10); //are they both equal? true
+console.log(10 != 5); //are they different? true
+console.log(10 != 10); //are they different? false
+
+// logical operators
+// AND && shift-7 true when both are true
+console.log(3>2 && 1>5); //true and false = false
+console.log(3>2 && 1<5); //true and true = true
+console.log(3<2 && 1<5); //false and true = false
+console.log(3<2 && 1>5); //false and false = false
+// OR || shift-\ (next to enter key)
+//true when at least one was true
+console.log(3>2 || 1>5); //true or false =  true
+console.log(3>2 || 1<5); //true or true =  true
+console.log(3<2 || 1<5); //false or true =  true
+console.log(3<2 || 1>5); //false or false =  false
+
+// **ternary operators use three values
+console.log(true ? 1 : 2);
+console.log(false ? 1 : 2);
+console.log(3>2 ? "yes it is true" : "no, it is not true");
+console.log(3<2 ? "yes it is true" : "no, it is not true");
+console.log(1+1 == 2 && 10 * 10 > 50);
+
+// Order of Operations
+// Everything Else (happens first)
+// Comparison Operators
+// AND
+// OR (happens last)
+
+console.log( 1 + 1 == 3 && 10 * 12 > 100);
+console.log( 1 > 0 && 10 <= 100);
+console.log( 1 > 0 && 10 >= 100);
+console.log( 1 > 0 && 10 == 100);
+console.log( 1 > 0 || 10 <= 100);
+console.log( 1 > 0 || 10 >= 100);
+console.log( 1 > 0 || 10 == 100);
+
+// 2 == 3 && 120 > 100
+// false and true
+// false
+
+console.log( (1 + 1 == 3) && (10 * 12 > 100));
+console.log( ((1 + 1) == 3) && ((10 * 12) > 100));
---
+++ types.js
@@ -0,0 +1,34 @@
+console.log(7);
+// this is a comment
+
+// ** numbers **
+// integers
+console.log(-3);
+console.log(0);
+console.log(3);
+
+// floating point number
+console.log(3.14);
+console.log(100.045);
+
+// ** strings **
+// characters, letters, symbols, spaces
+console.log("hello");
+console.log('hello');
+console.log(`hello`);
+
+// arithmetic operators (math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2);
+console.log(14/2);
+console.log(14%2);
+console.log(15%10);
+
+// concatenation: combine strings
+console.log("My name"+" is Khloe.");
+console.log('My'+' age'+' is'+' 16.');
+console.log('Your age is\n17.');
+
+// template literal - plug in a calculated value
+console.log(`15 divided my 10 = ${15/10}`);
